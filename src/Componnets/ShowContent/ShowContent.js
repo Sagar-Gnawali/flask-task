e34b04ba9752cc3a9521359b33275b4941727f4c
Sagar Gnawali <sagargnawali2@gmail.com>
2021-06-11 18:55:52 +0545

    partially done

--- src/Componnets/ShowContent/ShowContent.js
+++ src/Componnets/ShowContent/ShowContent.js
@@ -4,30 +4,28 @@ import axios from 'axios';
 const url = "https://api.first.org/data/v1/news";
 const ShowContent = () => {
     const [data, setData] = useState([]);
-    const [post, setPost] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [posPerPage, setPostPerPage] = useState(5);
+    // const [post, setPost] = useState([]);
+    // const [loading, setLoading] = useState(false);
+    // const [currentPage, setCurrentPage] = useState(1);
+    // const [posPerPage, setPostPerPage] = useState(5);
     useEffect(() => {
         const fetchData = async () => {
             const response = await axios.get(url);
-            console.log(response['data'].data);
             setData(response['data'].data.splice(0,4));
         }
         fetchData();
     }, []);
-    const indexOfLastPost = currentPage * posPerPage;
-    const indexOfFirstPost = indexOfLastPost - posPerPage;
-    const currentPost = data.slice(indexOfFirstPost, indexOfLastPost);
-    console.log('current post is ====', currentPost);
+    // const indexOfLastPost = currentPage * posPerPage;
+    // const indexOfFirstPost = indexOfLastPost - posPerPage;
+    // const currentPost = data.slice(indexOfFirstPost, indexOfLastPost);
+    // console.log('current post is ====', currentPost);
     // setPost(currentPost])
     return (
         <div>
 
             {
                 data.map((item) => (
-                    <>
-                        <div className="main">
+                        <div className="main" key={item.id}>
                             <div className="container">
                                 <div key={item.id}>
                                     <h3 className="title">{item.title}</h3>
@@ -36,14 +34,13 @@ const ShowContent = () => {
                                 </div>
                             </div>
                         </div>
-                    </>
                 ))
             }
-            <div class="pagination">
-                <a href="#" className="active">1</a>
-                <a href="#" >2</a>
-                <a href="#">3</a>
-                <a href="#">&raquo;</a>
+            <div className="pagination">
+                <a href="!#" className="active">1</a>
+                <a href="!#" >2</a>
+                <a href="!#">3</a>
+                <a href="!#">&raquo;</a>
             </div>
         </div>
     )
